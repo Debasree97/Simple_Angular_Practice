a5453f810ea67c6e1bda4b5681145f1b1540881e
Debasree97 <atd21821@gmail.com>
2022-07-04 01:09:55 +0600

    reactive form data console logged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'simple-project';
+  currencyForm = new FormGroup({
+    countryName: new FormControl(''),
+    currencyName: new FormControl(''),
+    currencyCode: new FormControl(''),
+    activeStat: new FormControl(''),
+  });
+
+  activeStatus: string = 'Inactive';
+
+  onSubmit() {
+    console.log(this.currencyForm.value);
+  }
+
+  radioActive(event: any) {
+    this.activeStatus = event.target.value;
+    if (event.target.value) {
+      this.activeStatus = 'Active';
+    }
+  }
 }
